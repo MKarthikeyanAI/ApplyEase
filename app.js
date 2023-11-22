7adee83402f4efd2ca5082672298c19d1864e8be
MKarthikeyanAI <mkarthikeyan00100010@gmail.com>
2023-11-22 15:43:03 +0530

    sgd

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+try{
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname,'index.html'));
 });
+}
+
+catch(error){
+    console.log("error",error);
+}
 
 // Serve static files (like CSS and JavaScript)
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
