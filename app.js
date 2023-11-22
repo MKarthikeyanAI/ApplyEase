cb726b73d6db478519b0a336d8ca929e08f88ffd
MKarthikeyanAI <mkarthikeyan00100010@gmail.com>
2023-11-22 15:31:34 +0530

    yeessss

--- app.js
+++ app.js
@@ -1,44 +1,16 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
-
-const server = http.createServer((req, res) => {
-  let filePath = '.' + req.url;
-  if (filePath === './') {
-    filePath = '..','..','index.html';
-  }
-
-
-  const extname = String(path.extname(filePath)).toLowerCase();
-  const contentType = {
-    '.html': 'text/html',
-    '.css': 'text/css',
-    '.js': 'text/javascript',
-    '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpg',
-    '.gif': 'image/gif',
-    '.svg': 'image/svg+xml',
-  }[extname] || 'application/octet-stream';
-
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        res.writeHead(404);
-        res.end('404 Not Found');
-      } else {
-        res.writeHead(500);
-        res.end(`Server Error: ${err.code}`);
-      }
-    } else {
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(content, 'utf-8');
-    }
-  });
-});
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Serve static files (like CSS and JavaScript)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
